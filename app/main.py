daee068926a6f1e365263ce63faaacad2dc28a2d
ktny <katsunori.t10@gmail.com>
2023-01-07 15:41:04 +0900

    implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -11,11 +11,17 @@ def main():
         threading.Thread(target=handle_connection, args=(client_socket,)).start()
 
 
-def handle_connection(client_connection: socket.socket):
+def handle_connection(client_socket: socket.socket):
     while True:
         try:
-            client_connection.recv(1024)
-            client_connection.send(b"+PONG\r\n")
+            message = client_socket.recv(1024)
+            tokens = message.decode().split("\r\n")
+
+            if tokens[2].upper() == "ECHO":
+                client_socket.send(f"+{tokens[4]}".encode())
+            else:
+                client_socket.send(b"+PONG\r\n")
+
         except ConnectionError:
             break
 

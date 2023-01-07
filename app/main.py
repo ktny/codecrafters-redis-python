f5f6f3dc20a2e970794fc5bb2b2eb2e526acdeaf
ktny <katsunori.t10@gmail.com>
2023-01-07 17:13:28 +0900

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from time import time
+from typing import List
 
 store = {}
 
@@ -20,36 +21,42 @@ def handle_connection(client_socket: socket.socket):
             message = client_socket.recv(1024)
             if not message:
                 break
-
             tokens = message.decode().split("\r\n")
-
-            match tokens[2].upper():
-                case "PING":
-                    client_socket.send(bytes("+PONG\r\n", "utf-8"))
-                case "ECHO":
-                    client_socket.send(bytes(f"+{tokens[4]}", "utf-8"))
-                case "SET":
-                    key = tokens[4]
-                    value = tokens[6]
-                    expiry = None
-
-                    # set expirty if exists PX
-                    if len(tokens) > 8:
-                        expiry = int(tokens[10]) + int(time() * 1000)
-
-                    store[key] = (value, expiry)
-                    client_socket.send(bytes("+OK\r\n", "utf-8"))
-                case "GET":
-                    key = tokens[4]
-                    value, expiry = store[key]
-
-                    if expiry is None or time() * 1000 < expiry:
-                        client_socket.send(bytes(f"${len(value)}\r\n{value}\r\n", "utf-8"))
-                    else:
-                        client_socket.send(bytes("$-1\r\n", "utf-8"))
-
+            response = process_tokens(tokens)
+            client_socket.send(bytes(response, "utf-8"))
         except ConnectionError:
             break
+
+
+def process_tokens(tokens: List[str]) -> str:
+    command = tokens[2].upper()
+
+    match command:
+        case "PING":
+            return "+PONG\r\n"
+        case "ECHO":
+            return f"+{tokens[4]}\r\n"
+        case "SET":
+            key = tokens[4]
+            value = tokens[6]
+            expiry = None
+
+            # set expirty if exists PX
+            if len(tokens) > 8:
+                expiry = int(tokens[10]) + int(time() * 1000)
+
+            store[key] = (value, expiry)
+            return "+OK\r\n"
+        case "GET":
+            key = tokens[4]
+            value, expiry = store[key]
+
+            if expiry is None or time() * 1000 < expiry:
+                return f"${len(value)}\r\n{value}\r\n"
+            else:
+                return "$-1\r\n"
+
+    raise ValueError("Unknown Command")
 
 
 if __name__ == "__main__":
